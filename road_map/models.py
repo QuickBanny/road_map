a3a77c1e5d3e46cc4751f5e5d2bf9a45f4bfb5c1
QuickBanny <maiorovpavel@gmail.com>
2020-08-20 11:17:20 +0500

    new style and model

--- road_map/models.py
+++ road_map/models.py
@@ -4,19 +4,31 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Status(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class MainQuest(models.Model):
     name = models.CharField(default='Quest', max_length=50)
     crt_date = models.DateField(auto_created=True)
 
+    def __str__(self):
+        return self.name
+
 
 class MainBlock(models.Model):
     quest = models.ForeignKey(MainQuest, on_delete=models.CASCADE, related_name='blocks')
     name = models.CharField(default='Block', max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Events(models.Model):
@@ -24,9 +36,9 @@ class Events(models.Model):
     name = models.CharField(default='Event', max_length=50)
     main_img = models.ImageField(upload_to='events_img')
     #category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    category = models.OneToOneField(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     #status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    status = models.OneToOneField(Status, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
     crt_date = models.DateField(auto_now_add=True)
     upd_date = models.DateField(auto_now=True)
     desc = models.TextField()
