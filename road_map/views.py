cd278ed43129212b5c0d0a0ffcc43eb86e9cd434
quickbanny <maiorovpavel@gmail.com>
2020-08-27 13:32:12 +0600

    add style bugs

--- road_map/views.py
+++ road_map/views.py
@@ -11,18 +11,21 @@ def first(request):
                                      "block_name2": ["event1", "event2"]}
                      }
 
-    dick_quests = {}
+    dict_quests = {}
     quests = MainQuest.objects.all()
     blocks = MainBlock.objects.all()
     for quest in quests:
         event_by_quest = Events.objects.filter(quest__name=quest.name)
-        dick_blocks = {}
+        dict_blocks = {}
         for block in blocks:
             event_by_block = event_by_quest.filter(block__name=block.name)
-            dick_blocks[block.name] = event_by_block
-        dick_quests[quest.name] = dick_blocks
+            dict_blocks[block.name.replace(' ', '_')] = {'name': block.name,
+                                                         'events': event_by_block,
+                                                         'count_events': len(event_by_block)}
+        dict_quests[quest.name.replace(' ', '_')] = {'name': quest.name,
+                                                     'blocks': dict_blocks}
 
-    return render(request, template_name='content.html', context={'context': dick_quests,
+    return render(request, template_name='content.html', context={'context': dict_quests,
                                                                   'quests': quests,
                                                                   'blocks': blocks})
 
